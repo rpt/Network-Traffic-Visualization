f968f8be139968a37c4990eebdc36ecb24938fc7
Krzysztof Rutka <rptmail@gmail.com>
2010-04-03 21:10:40 +0200

    Some changes... I don't remember.

--- parsers/sqlite2gv-ip-port.py
+++ parsers/sqlite2gv-ip-port.py
@@ -9,7 +9,7 @@ database = sys.argv[1]
 conn = sqlite3.connect(database)
 c = conn.cursor()
 
-conn.execute('create temporary table tmp_packets as select ip_src, ip_dst, port_src, port_dst, count(*) as count, sum(length) as length, trans_proto from packet_eth_ipv4 where mac_dst is not \'ff:ff:ff:ff:ff:ff\' and mac_dst is not \'00:00:00:00:00:00\' and mac_dst not like \'01:00:5e:%\' and ip_src is not \'0.0.0.0\' group by ip_src, ip_dst, port_src, port_dst, trans_proto;')
+conn.execute("create temporary table tmp_packets as select ip_src, ip_dst, port_src, port_dst, count(*) as count, sum(length) as length, trans_proto from packet_eth_ipv4 where mac_dst is not 'ff:ff:ff:ff:ff:ff' and mac_dst is not '00:00:00:00:00:00' and mac_dst not like '01:00:5e:%' and ip_src is not '0.0.0.0' group by ip_src, ip_dst, port_src, port_dst, trans_proto;")
 
 print 'digraph foo {'
 print '\tgraph [\n\t\trankdir = "LR"\n\t\toverlap = "false"\n\t\tsplines = "true"\n\t];'
@@ -20,13 +20,13 @@ print '\tnode [\n\t\tfontsize = "14"\n\t];'
 #counts = map ((lambda x: x[0]), c.fetchall())
 #pen_width = pen_selector(10, sorted(counts))
 
-c.execute('select ip_src, ip_dst, port_src, port_dst, trans_proto, count, length from tmp_packets;')
+c.execute("select ip_src, ip_dst, port_src, port_dst, trans_proto, count, length from tmp_packets;")
 for ip_src, ip_dst, port_src, port_dst, photo, count, length in c:
-#	print '\t"%s" [' % mac
+	print '\t"%s" [' % ip_src
 #	print '\t\tlabel = "<mac> %s' % mac,
 #	print '"\n\t];'
-	print '\t%s -> %s [' % (ip_src, ip_dst)
-	print '\t\tlabel = "%s"' % port_dst
+#	print '\t%s -> %s [' % (ip_src, ip_dst)
+#	print '\t\tlabel = "%s"' % port_dst
 #	print '\t\tpenwidth = "%f"' % (pen_width(count)+1)
 	print '\t];'
 
